a0a5dbf17a5f8a3b9a12f258e3ab5abfc1dc3cc6
lukmie <lmienicki@gmail.com>
2019-11-09 15:19:25 +0100

    first commit

---
+++ src/main/java/com/lukmie/restmovieapp/RestMovieAppApplication.java
@@ -0,0 +1,13 @@
+package com.lukmie.restmovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMovieAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestMovieAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lukmie/restmovieapp/RestMovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukmie.restmovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMovieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
